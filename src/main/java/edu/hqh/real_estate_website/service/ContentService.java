a00e007f6b833156d22bec5aab206f7bf9585702
LVQuang <ql7769663@gmail.com>
2024-04-25 19:58:04 +0700

    Backup

--- src/main/java/edu/hqh/real_estate_website/service/ContentService.java
+++ src/main/java/edu/hqh/real_estate_website/service/ContentService.java
@@ -1,5 +1,10 @@
 package edu.hqh.real_estate_website.service;
 
+import edu.hqh.real_estate_website.dto.request.ContentRequest;
+import edu.hqh.real_estate_website.dto.response.ContentResponse;
+import edu.hqh.real_estate_website.entity.Content;
+import edu.hqh.real_estate_website.enums.ErrorCode;
+import edu.hqh.real_estate_website.exception.AppException;
 import edu.hqh.real_estate_website.mapper.ContentMapper;
 import edu.hqh.real_estate_website.repository.ContentRepository;
 import lombok.AccessLevel;
@@ -14,5 +19,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class ContentService {
     ContentRepository contentRepository;
-    ContentMapper contentMapper;    
+    ContentMapper contentMapper;
+
+    public ContentResponse getById(String id) {
+        Content content = contentRepository.findById(id).orElseThrow(()
+                -> new AppException(ErrorCode.ITEM_DONT_EXISTS));
+        return contentMapper.toResponse(content);
+    }
 }
