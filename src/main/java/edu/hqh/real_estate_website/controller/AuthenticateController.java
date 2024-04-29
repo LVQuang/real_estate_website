ee2acda95394acec1e949c079aa14fb9b3187080
Le Vu Phuong Quang <92104788+LVQuang@users.noreply.github.com>
2024-04-29 23:31:10 +0700

    SE19: Authorization with thymeleaf

--- src/main/java/edu/hqh/real_estate_website/controller/AuthenticateController.java
+++ src/main/java/edu/hqh/real_estate_website/controller/AuthenticateController.java
@@ -55,7 +55,7 @@ public class AuthenticateController {
         else
         {
             httpRequest.getSession().setAttribute("myToken", authentication.getToken());
-            return "index";
+            return "redirect:/auth/test";
         }
     }
 
