d13b9bce79a3d6b3d29d50ef0f8493ee9b20bcc3
Le Vu Phuong Quang <92104788+LVQuang@users.noreply.github.com>
2024-04-30 21:02:49 +0700

    SE25: Contacts List Render

---
+++ src/main/java/edu/hqh/real_estate_website/controller/ContactController.java
@@ -0,0 +1,42 @@
+package edu.hqh.real_estate_website.controller;
+
+import edu.hqh.real_estate_website.service.ContactService;
+import edu.hqh.real_estate_website.service.PostService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@RequestMapping("/contact")
+@Controller
+public class ContactController {
+    ContactService contactService;
+
+    @GetMapping("/{pageNumber}")
+    String getContact(Model model,
+                   @RequestParam(name = "page",
+                           required = false, defaultValue = "1") Integer pageNumber
+    )
+    {
+        if (pageNumber == null)
+            pageNumber = 1;
+        var result = contactService.getAllContactsPage(pageNumber);
+        var contacts = result.getContent();
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("totalPages", result.getTotalPages());
+        if(result.getTotalPages() == 0) {
+            return "/layout/contacts";
+        }
+        if(result.getTotalPages() <= pageNumber)
+            return "redirect:/contact/1?outPage";
+        return "/layout/contacts";
+    }
+}
