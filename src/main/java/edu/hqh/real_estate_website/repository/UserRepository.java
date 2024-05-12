a194f2585e6e82cf4310c57f8e889ce670ad2154
LVQuang <ql7769663@gmail.com>
2024-05-12 16:34:32 +0700

    HF: Optimize previous project

--- src/main/java/edu/hqh/real_estate_website/repository/UserRepository.java
+++ src/main/java/edu/hqh/real_estate_website/repository/UserRepository.java
@@ -1,11 +1,7 @@
 package edu.hqh.real_estate_website.repository;
 
 import edu.hqh.real_estate_website.entity.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
